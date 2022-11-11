2e3475ff59e7e00ad5031c881edb363184355f82
ThongTrn <thongminh.tran@sjsu.edu>
2022-11-11 15:05:54 -0800

    added styles

--- sketch.js
+++ sketch.js
@@ -1,7 +1,32 @@
+let drawing =[];
+
 function setup() {
-  createCanvas(400, 400);
+  createCanvas(600, 600);
+  background(220);
+  strokeWeight (5);
+  noFill();
 }
 
 function draw() {
-  background(220);
+
+  if (mouseIsPressed) {
+      // line(mouseX, mouseY, pmouseX, pmouseY);
+      drawing.push ([mouseX, mouseY]);
+  } 
+     
 }
+function keyTyped(){
+
+  if (key === 's') { 
+    //save drawing
+    saveCanvas ('fileName', 'png');
+  } else if (key === 'd') {
+    //display image
+    for (let i = 0; i < drawing.length - 1; i++) {
+    // line(drawing[i][0], drawing [i][1],drawing [i + 1][0], drawing [i + 1][1]);
+    curveVertex (drawing[i][0], drawing [i][1])
+    }
+   endShape();
+  }
+  return false;
+  }
